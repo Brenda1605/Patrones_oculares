564e0523f26f49a21eda4b61c57be1cce035763f
Ulivego <ulivego@hotmail.com>
2020-09-17 15:36:25 -0700

    Metodo Prewitt con mejoras

--- Convolucion.py
+++ Convolucion.py
@@ -1,18 +1,20 @@
-def convolucion(a, nombreKernel):
-     import cv2
-     import numpy as np
+import cv2
+import numpy as np
+
+def convolucion(kernel, nombreKernel):
 
      ruta = 'ojo.jpg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
 
-     kernel = np.asarray(a)
-
      filtro = cv2.filter2D(im, -1, kernel)
 
      nombreKernel = 'Convolucion ' + nombreKernel
-     cv2.imshow(nombreKernel ,filtro)
+     cv2.namedWindow(nombreKernel, cv2.WINDOW_KEEPRATIO)
+     cv2.imshow(nombreKernel, filtro)
      cv2.waitKey(0)
      cv2.destroyAllWindows
 
-convolucion([[1,1,1],[0,0,0],[-1,-1,-1]], "Prewitt X")
-convolucion([[-1,0,1],[-1,0,1],[-1,0,1]], "Prewitt Y")
+prewittKernelX = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+prewittKernelY = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+convolucion(prewittKernelX, "Prewitt X")
+convolucion(prewittKernelY, "Prewitt Y")
