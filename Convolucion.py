c4141f7a9166867d9e927ced5e7f437edfc24fcb
GSLeonardo <leo_galindo_01@hotmail.com>
2020-09-17 13:52:27 -0500

    Kernel arreglado, se hace la conversión a numpy array

--- Convolucion.py
+++ Convolucion.py
@@ -1,6 +1,7 @@
+import cv2
+import numpy as np
+
 def convolucion(a, nombreKernel):
-     import cv2
-     import numpy as np
      
      ruta = 'ojo.jpg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
@@ -15,9 +16,8 @@ def convolucion(a, nombreKernel):
      cv2.waitKey(0)
      cv2.destroyAllWindows
 
-cannyKernel = [[2,4,5,4,2],[4,9,12,9,4],[5,12,15,12,5],[4,9,12,9,4],[2,4,5,4,2]]
-for i in cannyKernel:
-     for j in i:
-          j /= 159
+cannyKernel = np.array([[2,4,5,4,2],[4,9,12,9,4],[5,12,15,12,5],[4,9,12,9,4],[2,4,5,4,2]])
+cannyKernel = cannyKernel/159
+
 convolucion(cannyKernel, "Canny")
 
