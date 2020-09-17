e34a8c4ff02db49d6bad336c8ea9433ddbf909c1
Brenda1605 <A01632227@itesm.mx>
2020-09-17 17:41:53 -0500

    Modificación en parametros función

--- Convolucion1.py
+++ Convolucion1.py
@@ -9,7 +9,7 @@ def convolucion(kernel, nombreKernel):
     kernelBordes = np.array([[-2,-1,0],[-1,1,1],[0,1,2]])
     prefiltro = cv2.filter2D(im, -1, kernelBordes)
 
-    filtro = cv2.filter2D(im, -1, kernel)
+    filtro = cv2.filter2D(prefiltro, -1, kernel)
 
     nombreKernel = 'Convolucion ' + nombreKernel
     cv2.namedWindow(nombreKernel, cv2.WINDOW_KEEPRATIO)
