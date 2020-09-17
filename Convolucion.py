7d267618ac1fef4edf7b714a224c3639f2caafdb
GSLeonardo <leo_galindo_01@hotmail.com>
2020-09-17 17:24:33 -0500

    Prueba y prefiltro para resaltar bordes

--- Convolucion.py
+++ Convolucion.py
@@ -3,10 +3,13 @@ import numpy as np
 
 def convolucion(kernel, nombreKernel):
      
-     ruta = 'ojo.jpg'
+     ruta = 'ojoPrueba.jpeg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
      
-     filtro = cv2.filter2D(im, -1, kernel)
+     kernelBordes = np.array([[-2,-1,0],[-1,1,1],[0,1,2]])
+     prefiltro = cv2.filter2D(im, -1, kernelBordes)
+
+     filtro = cv2.filter2D(prefiltro, -1, kernel)
      
      nombreKernel = 'Convolucion: ' + nombreKernel
      cv2.namedWindow(nombreKernel, cv2.WINDOW_KEEPRATIO)
@@ -19,3 +22,8 @@ def convolucion(kernel, nombreKernel):
 cannyKernel = np.array([[2,4,5,4,2],[4,9,12,9,4],[5,12,15,12,5],[4,9,12,9,4],[2,4,5,4,2]])/159
 convolucion(cannyKernel, "Canny")
 
+sobelMxKernel = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+convolucion(sobelMxKernel,"Sobel Mx")
+
+sobelMyKernel = np.array([[1,2,1],[0,0,0],[-1,-2,-1]]) 
+convolucion(sobelMyKernel, "Sobel My")
