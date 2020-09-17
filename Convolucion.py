5bb93fb937ee67daec91997db1a1d82bf37facfe
GSLeonardo <leo_galindo_01@hotmail.com>
2020-09-17 13:07:13 -0500

    Prueba de Función

--- Convolucion.py
+++ Convolucion.py
@@ -2,14 +2,17 @@ def convolucion(a, nombreKernel):
      import cv2
      import numpy as np
      
-     ruta = 'perro1.png'
+     ruta = 'ojo.jpg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
      
      kernel = np.asarray(a)
      
      filtro = cv2.filter2D(im, -1, kernel)
      
-     nombreKernel = 'Convolución ' + nombreKernel
+     nombreKernel = 'Convolucion ' + nombreKernel
      cv2.imshow(nombreKernel ,filtro)
      cv2.waitKey(0)
      cv2.destroyAllWindows
+
+convolucion([[1,1,1],[1,1,1],[1,1,1]], "Kernel")
+
