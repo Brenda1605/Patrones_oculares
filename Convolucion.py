276bc25b2020ac59da0d35fffa098bf3d9bdf3d4
Brenda1605 <A01632227@itesm.mx>
2020-09-17 16:47:25 -0500

    Cambio en función convolución, acomodo de librerías importadas

--- Convolucion.py
+++ Convolucion.py
@@ -1,18 +1,19 @@
-def convolucion(a, nombreKernel):
-     import cv2
-     import numpy as np
-     
+import cv2 
+import numpy as np
+
+def convolucion(kernel, nombreKernel):
+
      ruta = 'ojo.jpg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
-     
-     kernel = np.asarray(a)
      
      filtro = cv2.filter2D(im, -1, kernel)
      
      nombreKernel = 'Convolucion ' + nombreKernel
+     cv2.namedWindow(nombreKernel, cv2.WINDOW_KEEPRATIO)
      cv2.imshow(nombreKernel ,filtro)
      cv2.waitKey(0)
      cv2.destroyAllWindows
 
-convolucion([[-1,0,1],[-2,0,2],[-1,0,1]], "Sobel Mx")
+sobelMxKernel = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])/159
+convolucion(sobelMxKernel,"Sobel Mx")
 
