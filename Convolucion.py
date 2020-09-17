a2bac0cd1702716b3c1b315924c1fd3f133384a9
Diego Ortiz <A01552000@itesm.mx>
2020-09-17 17:47:37 -0500

    Una solo archivo

--- Convolucion.py
+++ Convolucion.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def convolucion(kernel, nombreKernel):
-    ruta = 'ojoPrueba.jpeg'
+    ruta = 'ojo.jpg'
     im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
 
     kernelBordes = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
@@ -18,8 +18,7 @@ def convolucion(kernel, nombreKernel):
     cv2.destroyAllWindows
 
 
-scharrKernelHorizontal = [[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]]
+scharrKernelHorizontal = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
 convolucion(scharrKernelHorizontal, "Scharr")
 
-scharrKernelVertical = [[-3, -10, -3], [0, 0, 0], [3, 10, 3]]
-convolucion(scharrKernelVertical, "Scharr")
+
