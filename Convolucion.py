ab8182acaca5ab7c64c4df65138657f474991951
GSLeonardo <leo_galindo_01@hotmail.com>
2020-09-17 13:02:51 -0500

    Función Convolución arreglada

--- Convolucion.py
+++ Convolucion.py
@@ -1,14 +1,15 @@
-def convolucion(a):
+def convolucion(a, nombreKernel):
      import cv2
      import numpy as np
+     
      ruta = 'perro1.png'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
-
-     a = [[-1,0,1],
-          [-1,0,1],
-          [-1,0,1]]
+     
      kernel = np.asarray(a)
+     
      filtro = cv2.filter2D(im, -1, kernel)
-     cv2.imshow('Convolucion',filtro)
+     
+     nombreKernel = 'Convolución ' + nombreKernel
+     cv2.imshow(nombreKernel ,filtro)
      cv2.waitKey(0)
      cv2.destroyAllWindows
