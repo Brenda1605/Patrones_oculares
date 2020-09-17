da9d10fa97de57060b19f07ed98b79a6e2893564
Ulivego <ulivego@hotmail.com>
2020-09-17 11:55:07 -0700

    Detección de bordes usando Prewitt en Y y en X

--- Convolucion.py
+++ Convolucion.py
@@ -1,18 +1,18 @@
 def convolucion(a, nombreKernel):
      import cv2
      import numpy as np
-     
+
      ruta = 'ojo.jpg'
      im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
-     
+
      kernel = np.asarray(a)
-     
+
      filtro = cv2.filter2D(im, -1, kernel)
-     
+
      nombreKernel = 'Convolucion ' + nombreKernel
      cv2.imshow(nombreKernel ,filtro)
      cv2.waitKey(0)
      cv2.destroyAllWindows
 
-convolucion([[1,1,1],[1,1,1],[1,1,1]], "Kernel")
-
+convolucion([[1,1,1],[0,0,0],[-1,-1,-1]], "Prewitt X")
+convolucion([[-1,0,1],[-1,0,1],[-1,0,1]], "Prewitt Y")
