513aafefd4756facbde4371b04d05ab68b18116e
Diego Ortiz <A01552000@itesm.mx>
2020-09-18 10:09:41 -0500

    Agregue vertical

--- Convolucion.py
+++ Convolucion.py
@@ -1,3 +1,7 @@
+import cv2
+import numpy as np
+
+
 # This function called convolucion receives two parameters which are the kernel
 # that the function is going to use and the name of the filter/kernel.  It uses
 # functions from the cv2 and numpy libraries to convert the image into an array
@@ -6,39 +10,42 @@
 # new array with the kernel that the funtion received.  Then we show the resulting
 # image after applying those kernels.
 
-def convolucion(kernel, nombreKernel):
-    ruta = 'ojo.jpg'
-    im = cv2.imread(ruta, cv2.IMREAD_GRAYSCALE)
+def convolution(kernel, kernelName):
+    path = "ojo.jpg"
+    im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
-    kernelBordes = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
-    prefiltro = cv2.filter2D(im, -1, kernelBordes)
+    kernelBorders = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]])
+    prefilter = cv2.filter2D(im, -1, kernelBorders)
 
-    filtro = cv2.filter2D(prefiltro, -1, kernel)
+    filtro = cv2.filter2D(prefilter, -1, kernel)
 
-    nombreKernel = 'Convolucion: ' + nombreKernel
-    cv2.namedWindow(nombreKernel, cv2.WINDOW_KEEPRATIO)
-    cv2.imshow(nombreKernel, filtro)
+    kernelName = "Convolution: " + kernelName
+    cv2.namedWindow(kernelName, cv2.WINDOW_KEEPRATIO)
+    cv2.imshow(kernelName, filtro)
     cv2.waitKey(0)
     cv2.destroyAllWindows
 
 
+# Variables hold the kernel for each corresponding filter
+# Each kernel is assigned to the function convolution, with its corresponding name for use in display
+
+cannyKernel = np.array([[2, 4, 5, 4, 2], [4, 9, 12, 9, 4], [5, 12, 15, 12, 5], [4, 9, 12, 9, 4], [2, 4, 5, 4, 2]]) / 159
+convolution(cannyKernel, "Canny")
+
 scharrKernelHorizontal = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
-convolucion(scharrKernelHorizontal, "Scharr")
+convolution(scharrKernelHorizontal, "Scharr horizontal")
 
 scharrKernelVertical = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
-convolucion(scharrKernelVertical, "Scharr")
+convolution(scharrKernelVertical, "Scharr vertical")
 
-cannyKernel = np.array([[2,4,5,4,2],[4,9,12,9,4],[5,12,15,12,5],[4,9,12,9,4],[2,4,5,4,2]])/159
-convolucion(cannyKernel, "Canny")
+sobelMxKernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+convolution(sobelMxKernel, "Sobel Mx")
 
-sobelMxKernel = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-convolucion(sobelMxKernel,"Sobel Mx")
+sobelMyKernel = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+convolution(sobelMyKernel, "Sobel My")
 
-sobelMyKernel = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
-convolucion(sobelMyKernel, "Sobel My")
+prewittKernelX = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+convolution(prewittKernelX, "Prewitt X")
 
-prewittKernelX = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
-convolucion(prewittKernelX, "Prewitt X")
-
-prewittKernelY = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
-convolucion(prewittKernelY, "Prewitt Y")
+prewittKernelY = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+convolution(prewittKernelY, "Prewitt Y")
